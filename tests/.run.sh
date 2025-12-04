87ac88ff1a25ce1a46e5c52204177a5f57aa4870
Miriamgarsol23 <miriamgarciasollo@gmail.com>
2025-12-04 14:26:35 +0100

    Add files via upload

--- tests/.run.sh
+++ tests/.run.sh
@@ -1,1 +1,4 @@
-
+touch tests//.timeout
+CMD="valgrind --leak-check=full /home/usuario/Descargas/UGRMPBase/NetBeansProjects/Boston4/dist/Debug/GNU-Linux/boston4  -c 1> tests//.out16 2>&1"
+eval $CMD
+rm tests//.timeout
