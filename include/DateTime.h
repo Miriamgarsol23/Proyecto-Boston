791d16373ddfa6526fe17fb25ecae131cbb72311
Miriamgarsol23 <miriamgarciasollo@gmail.com>
2025-12-04 14:32:24 +0100

    Update DateTime.h

--- include/DateTime.h
+++ include/DateTime.h
@@ -5,9 +5,7 @@
 
 /**
  * @file DateTime.h
- * @author Silvia Acid Carrillo <acid@decsai.ugr.es>
- * @author Andrés Cano Utrera <acu@decsai.ugr.es>
- * @author Luis Castillo Vidal <L.Castillo@decsai.ugr.es> 
+ * @author Miriam Garcia Sollo
  * 
  * Created on September 17, 2024, 5:43 PM
  */
