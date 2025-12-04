64af0dafce6aeccfea38563342dbdbbfe3d72119
Miriamgarsol23 <miriamgarciasollo@gmail.com>
2025-12-04 14:29:41 +0100

    Update CrimeCounter.cpp

--- src/CrimeCounter.cpp
+++ src/CrimeCounter.cpp
@@ -5,9 +5,7 @@
 
 /** 
  * @file:  CrimeCounter.cpp
- * @author Silvia Acid Carrillo <acid@decsai.ugr.es>
- * @author Andrés Cano Utrera <acu@decsai.ugr.es>
- * @author Luis Castillo Vidal <L.Castillo@decsai.ugr.es>
+ * @author Miriam Garcia Sollo
  * Created on September 24, 2024, 3:19 PM
  */
 
